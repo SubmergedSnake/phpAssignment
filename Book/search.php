e860200f01e91b34f6444a54b10699bcf0d70c95
Joonas Heikkinen <solarshadow.jh@gmail.com>
2017-04-25 10:01:36 +0300

    JSONSearch in progress

--- Book/search.php
+++ Book/search.php
@@ -1,33 +1,5 @@
 
 <?php
-require_once "user.php";
-
-date_default_timezone_set('Europe/Helsinki');
-
-if(isset($_COOKIE["username"])) {
-	$user = $_COOKIE["username"];
-} 
-
-if (isset ( $_POST ["insertuser"])) {
-	$user = new User($_POST['username']);
-	
-	$usererror = $user -> checkUsername();
-
-if(empty($usererror)){
-	
-	$currenttime = date("d.m.y H:i");
-	setcookie("username", $user->getUsername(), time()+(86400*7), "/");
-	setcookie("logintime", $currenttime, time()+(86400*7), "/");
-	header("location: index.php");
-	exit;
-
-}
-}
-else{
-	$user = new User();
-	$usererror = 0;
-}
-		
 ?>
 
 <html>
@@ -43,76 +15,109 @@ else{
 
 <title>The Fine Print</title>
 
+<style>
+label, input, textarea{float:none;margin-left:1em;}
+form{text-align:left;}
+button{margin-left:1.4em;margin-top:1em;}
+
+</style>
+
 </head>
 <body>
 	<div class="main">
-		
+
 		<header>
 			<h2>
 				Search <small><i class="fa fa-search" aria-hidden="true"></i></small>
 			</h2>
 			<nav class="subnav">
-				<li><a class="arrow" href="submitbook.php " class="nav">Submit a new book</i></a></li>
+				<li><a class="arrow" href="submitbook.php " class="nav">Submit a new
+						book</i>
+				</a></li>
 				<li><a class="arrow" href="allbooks.php " class="nav">All books</a></li>
 				<li><a class="arrow" href="settings.php " class="nav">Settings</a></li>
-				
-				
+
+
 
 			</nav>
 		</header>
 
-<div class="half">
-<form action="settings.php" method="POST">
-<h4><label for="username" style="width:100%;margin-bottom:1em;">Search for a book</label></h4>
-<input type="text" style="width:70%" name="searchparam" value="" ><button type="submit"  style="float:left;margin-left:0.5em;padding:0.3em;background:#00ff00">Search <i class="fa fa-search" aria-hidden="true"></i></button>
-
+		<div class="half" style="float:none">
+			<form action="settings.php" method="POST">
+				<h4>
+					Search for a book
+				</h4>				
+				<label class="searchlb">By Title<input type="radio" name="condition" value="title" /></label>
+				 <label class="searchlb">By Author<input type="radio" name="condition" value="author" /></label>
+				 
+				<label style="width:100%;padding-top:0.7em;">Search params<input
+					type="text" style="width: 70%" name="searchparam" value="" /></label>
 					
+					<button type="submit"
+					style="background: #00ff00">
+					Search <i class="fa fa-search" aria-hidden="true"></i>
+				</button>
+			</form>
+
+
+
+		</div>
+
+<!--  
+	<label>Search params<input
+					type="text" style="width: 70%" name="searchparam" value="" /></label>
 					
-</form>
+				<button type="submit"
+					style="background: #00ff00">
+					Search <i class="fa fa-search" aria-hidden="true"></i>
+				</button>
+-->
+
+		<div class="padtop1 pbsides2" id="results">Results here</div>
+
+	</div>
 
 
-</div>
 
-</div>
-	
-	
-	
-	
+
 	<div id="user">
-	<a href="/Eta1/Book" class="glowhome"><i
-			class="fa fa-home" aria-hidden="true"></i></a>
-			<p id="pipe">&verbar;</p>
-			
-<h4 id="cookiename" >
+		<a href="/Eta1/Book" class="glowhome"><i class="fa fa-home"
+			aria-hidden="true"></i></a>
+		<p id="pipe">&verbar;</p>
+
+		<h4 id="cookiename">
 
 
   
  
 <?php
 
-if (isset($_COOKIE["username"])) {
- $usernames = explode(" ", $_COOKIE["username"]);
- print($usernames[0]);
+if (isset ( $_COOKIE ["username"] )) {
+	$usernames = explode ( " ", $_COOKIE ["username"] );
+	print ($usernames [0]) ;
 }
 ?>
 
-<span id="usertooltip"><span>Full username:</span><?php foreach($usernames as $name){
-print(" " . $name);
-}?><br><span>Logged in since: <?php if(isset($_COOKIE['logintime'])){ print ($_COOKIE["logintime"]);}?></span></span>
+<span id="usertooltip"><span>Full username:</span><?php
 
-</h4>
+foreach ( $usernames as $name ) {
+	print (" " . $name) ;
+}
+?><br> <span>Logged in since: <?php if(isset($_COOKIE['logintime'])){ print ($_COOKIE["logintime"]);}?></span></span>
 
-
-
-
-			<div style="clear: both;"></div>
-</div>
+		</h4>
 
 
 
-<script src="../Jscript/dashboard.js">
+
+		<div style="clear: both;"></div>
+	</div>
+
+
+
+	<script src="../Jscript/dashboard.js">
 </script>
 
-	
+
 </body>
 </html>
